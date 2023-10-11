3f3351e409cb3c7dd928e7c1b5b465fb4d582464
Oubi12 <robiehbels@gmail.com>
2023-10-11 16:57:39 +0800

    additional to image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
   validates :title, :content, :address, presence: true
   has_many :comments
-  has_many :post_category_ships
+  has_many :post_category_ships,dependent: :restrict_with_error
+  has_many :comments, dependent: :restrict_with_error
   has_many :categories, through: :post_category_ships
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
 end
