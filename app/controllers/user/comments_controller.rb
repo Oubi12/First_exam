51e21f9651d506e6bc2e0072947f8ee21ff3be7f
Oubi12 <robiehbels@gmail.com>
2023-10-13 10:43:49 +0800

    authenticate user and add paginate in comments page and also edit associations

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,5 +1,6 @@
 class User::CommentsController < ApplicationController
+  before_action :authenticate_user!
   def index
-    @my_comments = current_user.comments
+    @my_comments = current_user.comments.includes(:post).order(created_at: :desc).page(params[:page]).per(5)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   validates :title, :content, :address, presence: true
-  has_many :comments
-  has_many :post_category_ships,dependent: :restrict_with_error
-  has_many :comments, dependent: :restrict_with_error
+  # has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :post_category_ships,dependent: :destroy
   has_many :categories, through: :post_category_ships
   belongs_to :user
 
